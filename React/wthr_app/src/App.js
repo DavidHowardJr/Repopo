b72cf68bc4bd6fadece25aebea5914844a4b92a9
DavidHowardJr <DavidHowardJr07@gmail.com>
2024-03-23 13:53:00 -0500

    well done

---
+++ React/wthr_app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Forecast from "./components/Forecast/Forecast.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WTHR</h1>
+      </header>
+      <main>
+        <Forecast />
+      </main>
+      <footer>
+        Page created by David Howard
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/wthr_app/src/components/Forecast/Forecast.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+
+const Forecast = () => {
+
+    let [responseObj, setResponseObj] = useState({});
+
+    function getForecast() {
+        const url = 'https://open-weather-map27.p.rapidapi.com/weather';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '4cdd02d352mshed0d6704df0b2e3p1847acjsnf8f05c978289',
+                'X-RapidAPI-Host': 'open-weather-map27.p.rapidapi.com'
+            }
+        };
+        
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            console.log(result);
+        } catch (error) {
+            console.error(error);
+        }
+
+        .then(response => (response.json())
+       .then(response => {
+        setResponseObj(response)
+
+}
+)});
+;
+
+return (
+    <div>
+        <h2>Find Current Weather Conditions</h2>
+        <div>
+            {JSON.stringify(responseObj)}
+        </div>
+        <button onClick={getForecast}>Get Forecast</button>
+    </div>
+)
+}
+
+export default Forecast;
