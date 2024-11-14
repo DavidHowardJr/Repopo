29d38bab0bcfeab47d07f4c089b1700827df8675
DavidHowardJr <DavidHowardJr07@gmail.com>
2024-11-14 17:09:39 -0600

    testing commit

---
+++ LearnVite/login-form/src/Login.jsx
@@ -0,0 +1,13 @@
+
+const Login = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="text-4xl font-bold text-center">Login</h1>
+                <form action=""></form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
