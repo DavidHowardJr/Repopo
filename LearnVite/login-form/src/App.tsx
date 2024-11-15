d6ec422e165bd8502cb3b58dfcfe18f838ee13cf
DavidHowardJr <DavidHowardJr07@gmail.com>
2024-11-14 19:59:48 -0600

    again

---
+++ LearnVite/login-form/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl text-blue-500 text-center">Welcome to TailwindCSS</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
