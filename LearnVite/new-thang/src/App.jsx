9bb64a33f4b72da77abfb794a4396a5319908730
DavidHowardJr <DavidHowardJr07@gmail.com>
2024-11-16 10:46:44 -0600

    changes

--- LearnVite/new-thang/src/App.jsx
+++ LearnVite/new-thang/src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+    {
+      index: 0,
+      url: 'https://images.unsplash.com/photo-1466721591366-2d5fba72006d?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      index: 1,
+      url: 'https://images.unsplash.com/photo-1493962853295-0fd70327578a?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      index: 2,
+      url: 'https://images.unsplash.com/photo-1518877593221-1f28583780b4?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      index: 3,
+      url: 'https://plus.unsplash.com/premium_photo-1669725687137-75f7a1377720?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      index: 4,
+      url: 'https://images.unsplash.com/photo-1485833077593-4278bba3f11f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-slate-200 h-screen flex items-center justify-center'>
+      <div className='w-[600px] h-[400px]'>
+        <img className='w-full h-full object-cover rounded-[20px]'
+        src={images[currentImage].url} 
+        alt="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
